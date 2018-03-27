5aae4878b8aa4824d17a43c1ece2ee87cedf5df9
prabhudayala <prabhudayala@gmail.com>
2018-03-27 12:01:56 +0530

    Update recurssion.py

--- python/recurssion.py
+++ python/recurssion.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+#Tower of hanoi
+#from to using auxilary == a to b using c
+def tower(n,a,b,c):
+    #if only one disk move it and stop
+    if n==1:
+        print("move disk 1 from %s to %s"%(a,b))
+        return
+    #move top n-1 disks from source to aux
+    tower(n-1,a,c,b)
+    print("move disk %d from peg %s to peg %s "%(n,a,b))
+    #move n-1 disks from b to c using a as auxilary
+    tower(n-1,c,b,a)
+tower(4,"T1","T3","T2")
+
